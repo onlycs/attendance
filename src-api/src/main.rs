148a0f7fc50dda31278733bdda948c6be27ecf1e
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-09-18 19:12:09 -0400

    bind to anywhere

--- src-api/src/main.rs
+++ src-api/src/main.rs
@@ -167,7 +167,7 @@ async fn main() -> Result<(), InitError> {
             .service(csv)
             .service(hours)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await?;
 
