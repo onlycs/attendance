ef0a0a9fc7571738f01e92466290e585714a9ce7
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-10-17 08:18:08 -0400

    NEXT_PUBLIC lol

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	publicRuntimeConfig: {
-		NODE_ENV: process.env.NODE_ENV,
-		API_URL: process.env.API_URL,
-		API_URL_DEV: process.env.API_URL_DEV || process.env.API_URL,
-	}
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import config from 'next/config';
 
-const { publicRuntimeConfig } = config();
-const { API_URL: API_URL_PROD, API_URL_DEV, NODE_ENV } = publicRuntimeConfig;
-export const API_URL = NODE_ENV === 'production' ? API_URL_PROD : API_URL_DEV;
-
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
